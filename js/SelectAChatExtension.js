9da7786ad720c2ed3b48d5848037be12ef631998
Aaron Martin <jumpercat25@gmail.com>
2021-11-20 09:39:58 -0600

    created css files. added getTestContent for testing chat variations.

--- js/SelectAChatExtension.js
+++ js/SelectAChatExtension.js
@@ -1,13 +1,33 @@
+function getTestContent(){
+  $.ajax({
+    url:'https://www.youtube.com/live_chat?is_popout=1&v=8NzYo0jmYek', 
+    method:'GET',
+    success: function(result){
+          
+          var begin = result.indexOf('liveChatTextMessageRenderer');
+          var end = result.indexOf('actionPanel');
+          var chat ='{'+ result.substring(begin-103, end-2)+'}';
+        addErr(chat);
+      }
+});
+}
+
 function getYTComments(){
     $.ajax({
             url:'https://www.youtube.com/live_chat?is_popout=1&v=8NzYo0jmYek', 
             method:'GET',
             success: function(result){
-              var begin = result.indexOf('liveChatTextMessageRenderer');
-              var end = result.indexOf('actionPanel');
+              var jsn;
+              try {
+                var begin = result.indexOf('liveChatTextMessageRenderer');
+                var end = result.indexOf('actionPanel');
 
-              var chat ='{'+ result.substring(begin-103, end-2)+'}';
-              var jsn = JSON.parse(chat);
+                var chat ='{'+ result.substring(begin-103, end-2)+'}';
+                jsn = JSON.parse(chat);
+              } catch (error) {
+                addErr(error);
+              }
+              
 
               jsn.actions.forEach(e => {
               
@@ -22,7 +42,7 @@ function getYTComments(){
                   window.localStorage.setItem(cId, JSON.stringify(comment));
                 }
                } catch (error) {
-                 
+                 //addErr(error);
                }
                 
               });
@@ -33,8 +53,8 @@ function getYTComments(){
   function addCmt(cmt){
     $("#comment").prepend($("#myTemplate").tmpl(cmt));
   }    
-  function addCmt2(cmt){
-    $("#tst2").append(cmt);
+  function addErr(cmt){
+    $("#err").append(cmt);
   }    
 
   function writeCurrent(commentId){
